7a7227bc2ced0e0a45bf897c59bb269ebf5ac0ea
Pheebes <phoebe.hardwicke@waterfield.com>
2015-10-15 11:47:04 -0500

    Despite creating a 500 server error and who knows what else this is generating text messages.

--- app/controllers/receive_texts_controller.rb
+++ app/controllers/receive_texts_controller.rb
@@ -1,14 +1,17 @@
 class ReceiveTextsController < ApplicationController
 
+  skip_before_action :verify_authenticity_token
   def index
 
   #need to add next two lines to config file
-  account_sid = "set_me"
-  auth_token = "set_me"
+  account_sid = ''
+  auth_token = ''
 
     @client = Twilio::REST::Client.new account_sid, auth_token
 
     @client.messages.list.each do |message|
+      puts message.body
+      puts message.body
       puts message.body
     end
   end
--- app/controllers/send_texts_controller.rb
+++ app/controllers/send_texts_controller.rb
@@ -1,23 +1,24 @@
 class SendTextsController < ApplicationController
 
+  skip_before_action :verify_authenticity_token
+
   def index
   end
 
   def send_text_message
-    number_to_send_to = params[:number_to_send_to]
 
     #move next three lines to config file
-    account_sid = "set_me"
-    auth_token = "set_me"
-    twilio_phone_number = "set_me"
+    account_sid = ''
+    auth_token = ''
+    twilio_phone_number = ''
 
     @client = Twilio::REST::Client.new account_sid, auth_token
 
-    @client.account.sms.messages.create(
+    @client.account.messages.create({
       :from => "+1#{twilio_phone_number}",
-      :to => number_to_send_to,
-      :body => "Testing..."
-    )
+      :to => "",
+      :body => "Testing...222"
+    })
 
   end
 end
