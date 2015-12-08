f50704148cfa5acefd02e859d0bbef61ed3b3877
Pheebes <phoebe.hardwicke@waterfield.com>
2015-12-08 15:22:52 -0600

    Untested updates... need to set up ngrok again

--- app/controllers/receive_texts_controller.rb
+++ app/controllers/receive_texts_controller.rb
@@ -1,8 +1,9 @@
 class ReceiveTextsController < ApplicationController
-
   skip_before_action :verify_authenticity_token
-  def index
+  before_action :set_store
+  before_action :set_user, only: [:respond]
 
+  def index
     @client = Twilio::REST::Client.new
     @client.messages.list.each do |message|
       puts message.body
@@ -10,9 +11,20 @@ class ReceiveTextsController < ApplicationController
   end
 
   def respond
-    @users = User.all
+    @users = @store.users.all
     @zip = params[:FromZip]
     @body = params[:Body]
     render 'respond.xml.erb', :content_type => 'text/xml'
   end
+
+  private
+     # Set store so app will be able to find users associated to the store
+    def set_store
+      @store = Store.find(params[:store_id])
+    end
+
+    # Set user which will be called before the respond action
+    def set_user
+      @user = @store.users.find(params[:id])
+    end
 end
