0af2cca14f8eeb0c9f5ac5b669becc550d664f2e
Pheebes <phoebe.hardwicke@waterfield.com>
2015-12-08 15:53:26 -0600

    So many things broke... need to add tests.

--- app/controllers/receive_texts_controller.rb
+++ app/controllers/receive_texts_controller.rb
@@ -1,7 +1,7 @@
 class ReceiveTextsController < ApplicationController
   skip_before_action :verify_authenticity_token
-  before_action :set_store
-  before_action :set_user, only: [:respond]
+  before_action :set_store, only: [:respond]
+
 
   def index
     @client = Twilio::REST::Client.new
@@ -13,18 +13,16 @@ class ReceiveTextsController < ApplicationController
   def respond
     @users = @store.users.all
     @zip = params[:FromZip]
-    @body = params[:Body]
+    @body = params[:Body].to_s
     render 'respond.xml.erb', :content_type => 'text/xml'
   end
 
   private
      # Set store so app will be able to find users associated to the store
     def set_store
-      @store = Store.find(params[:store_id])
+      @store = Store.find(1) #(params[:id])
     end
 
     # Set user which will be called before the respond action
-    def set_user
-      @user = @store.users.find(params[:id])
-    end
+
 end
